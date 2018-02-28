6193ab9717647d7d5df3cf4603fd6af96ffd74ef
shalindra shrestha <shalindra.shrestha@gmail.com>
2018-02-28 11:40:07 -0800

    Optimized and Production Built

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,39 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
-  entry: ['babel-polyfill','./lib/renderers/dom.js'],
+  entry: {
+    app:['./lib/renderers/dom.js'],
+    vendor:['babel-polyfill',
+            'react',
+            'react-dom',
+            'prop-types',
+            'axios',
+            'lodash.pickby'
+          ]
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude:/node_modules/, use: 'babel-loader' }
+      { 
+        test: /\.js$/, 
+        exclude:/node_modules/, 
+        use: {
+          loader: 'babel-loader',
+          options:{
+            presets: ['react', 'env', 'stage-2']
+          }
+        } }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+    })
+  ]
 };
 
 module.exports = config;
